1e58f1d84d557335ac178bd6f4bd072ca9f8fe94
Joona <joona114@hotmail.fi>
2025-07-12 01:02:00 +0300

    d

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
-        theme_color: '#22c55e',
+        theme_color: '#000000',
         icons: [
           {
             src: '/vite.svg',
